1600d01156ab3a08b11e7e8cb6289b9b1e676051
mat23795 <mat23795@hotmail.it>
2019-02-05 12:23:36 +0100

    modificati script e aggiunto file nothing a files/data

--- gcpScript/7.new_dataproc-run-job.sh
+++ gcpScript/7.new_dataproc-run-job.sh
@@ -3,7 +3,7 @@
 . 0.new_variables.sh
 
 ./90.log-time.sh "RUNNING SPARK JOB '${SCALA_RUNNABLE_CLASS}' OVER '${DATA_FILE}' DATA FILE ON '${DATAPROC_CLUSTER_NAME}' CLUSTER ..."
-gcloud dataproc jobs submit spark --cluster ${DATAPROC_CLUSTER_NAME} --region ${DATAPROC_CLUSTER_REGION} \
+gcloud dataproc jobs submit spark --id ${JOB_ID} --cluster ${DATAPROC_CLUSTER_NAME} --region ${DATAPROC_CLUSTER_REGION} \
       --class ${SCALA_RUNNABLE_CLASS} \
       --jars ${SCALA_JAR_FILE_FOR_JOB_SUBMIT} \
       --project ${GCP_PROJECT_ID} \
